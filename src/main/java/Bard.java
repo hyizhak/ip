db5d24b230ed3b6018244002f61312391721a6e8
yizhak <hyz0235@gmail.com>
2025-02-03 11:16:18 +0800

    Accomplish Level 0. Rename, Greet, Exit

---
+++ src/main/java/Bard.java
@@ -0,0 +1,13 @@
+public class Bard {
+    public static void main(String[] args) {
+        String horizontalLine =
+                "____________________________________________________________\n";
+        String startingLine = horizontalLine +
+                " Hello! I'm Bard\n" +
+                " What can I do for you?\n" +
+                horizontalLine +
+                " Bye. Hope to see you again soon!\n" +
+                horizontalLine;
+        System.out.println(startingLine);
+    }
+}
