fd73c2998f007b77190e7e1e4364de8270c39a6e
yizhak <hyz0235@gmail.com>
2025-02-03 11:19:50 +0800

    Accomplish Level 1. Echo

--- src/main/java/Bard.java
+++ src/main/java/Bard.java
@@ -5,9 +5,20 @@ public class Bard {
         String startingLine = horizontalLine +
                 " Hello! I'm Bard\n" +
                 " What can I do for you?\n" +
-                horizontalLine +
+                horizontalLine;
+        String endingLine = horizontalLine +
                 " Bye. Hope to see you again soon!\n" +
                 horizontalLine;
         System.out.println(startingLine);
+
+        // Read input from user
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        String input = scanner.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(horizontalLine + " " + input + "\n" + horizontalLine);
+            input = scanner.nextLine();
+        }
+
+        System.out.println(endingLine);
     }
 }
